2295c7269a718cc2fa562c352d1f8cdda7a9b78b
pointer-to-bios <yyb1738487169@163.com>
2022-06-27 22:04:37 +0800

    add do_create_frame()

--- BWLserver/reqrec.cc
+++ BWLserver/reqrec.cc
@@ -2,7 +2,7 @@
 
 extern std::map<bwl::id_t, bwl::__page *> pages; //页列表
 uint64_t pgcount;
-extern bwl::id_t current_pgid;                   //当前页
+extern bwl::id_t current_pgid; //当前页
 
 extern std::map<bwl::id_t, bwl::__frame *> frames; //窗口列表
 uint64_t frcount;
@@ -25,6 +25,23 @@ namespace bwl
         pages.erase(rp->arguments.a_del_page.pgid);
     }
 
+    void do_create_frame(req_pack *rp)
+    {
+        std::string frname = rp->arguments.a_create_frame.name;
+        __frame *fr = createFrame(frcount++,                                // fid设为已创建的frame数量
+                                  pages[rp->arguments.a_create_frame.pgid], //所属的页
+                                  frname,                                   //窗口名
+                                  rp->arguments.a_create_frame.size[0],     //
+                                  rp->arguments.a_create_frame.size[1],     //窗口大小
+                                  rp->arguments.a_create_frame.pos[0],      //
+                                  rp->arguments.a_create_frame.pos[1]       //窗口位置
+        );
+        fr->space = rp->arguments.a_create_frame.space;
+        frames[fr->fid] = fr;
+    }
+
+    //TODO 完成do_delete_frame()
+
     /**
      * @brief req信号接受函数
      *
@@ -42,6 +59,9 @@ namespace bwl
             break;
         case requests::del_page:
             do_delete_page(rp);
+            break;
+        case requests::create_frame:
+            do_create_frame(rp);
             break;
         default:
             break;
