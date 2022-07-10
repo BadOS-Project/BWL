4138c28038a687287147b1c5d7530efddaa99f9c
pointer-to-bios <yyb1738487169@163.com>
2022-07-10 20:10:14 +0800

    add function getBgBuffSize() in order to surpport background size

--- includes/bwl.hh
+++ includes/bwl.hh
@@ -1,12 +1,20 @@
 #ifndef __bwl_hh_
 #define __bwl_hh_
 
-
 #include "bsv.hh"
 
 namespace bwl
 {
 
+    class Page
+    {
+    private:
+        __page *page;
+
+    public:
+        Page();
+        ~Page();
+    };
 
 };
 
