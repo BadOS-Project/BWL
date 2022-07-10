4d9b47c06d673f51b99d926a2813cbb48e30fa7c
pointer-to-bios <yyb1738487169@163.com>
2022-07-10 21:24:26 +0800

    add some explannation

--- includes/bwl.hh
+++ includes/bwl.hh
@@ -5,6 +5,18 @@
 
 namespace bwl
 {
+    /**
+     * @brief 初始化函数
+     * 
+     * this function must be the first code in main.
+     * however we have not yet wrote codes to search which monitor you're using
+     * so you can only write ths function after some monitor getting codes.
+     * 
+     * 这个函数应该是主函数的第一条语句。
+     * 我们现在还没写获取当前显示器的代码，所以这个函数应该只出现在获取当前显示器的代码之后。
+     * 
+     * @param __monitor_device 
+     */
     void initBadWayland(std::string __monitor_device);
 };
 
