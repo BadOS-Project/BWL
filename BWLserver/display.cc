9569dc64839573ec85ba6ed8a2f9f53f35bd4c9f
pointer-to-bios <yyb1738487169@163.com>
2022-07-16 10:12:37 +0800

    package drm open and close option.

--- BWLserver/display.cc
+++ BWLserver/display.cc
@@ -1,7 +1,10 @@
 #include "display.hh"
+#include "../includes/bsv.hh"
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -17,9 +20,53 @@
 #include <time.h>
 #include <unistd.h>
 #include <dirent.h>
-#include <cstdlib>
 #include <signal.h>
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 
+struct display
+{
+    int fd;
+    drmModeRes *res;
+    drmModeConnector *conn;
+    uint32_t fbid;
+    char *buffer;
+    drm_mode_create_dumb create;
+} __display;
 
+namespace bwl
+{
+    void initDisplay(std::string __card_dvc)
+    {
+        __display.fd = open((std::string("/dev/dri/card") + __card_dvc).c_str(), O_RDWR | O_CLOEXEC); //打开显卡设备文件
+        /*获取drm信息*/
+        __display.res = drmModeGetResources(__display.fd);
+        uint32_t crtc_id = __display.res->crtcs[0];
+        uint32_t conn_id = __display.res->connectors[0];
+        __display.conn = drmModeGetConnector(__display.fd, conn_id);
+        setmonitor(__display.conn->modes[0].hdisplay, __display.conn->modes[0].vdisplay, 4);
+        __display.create.width = __display.conn->modes[0].hdisplay;
+        __display.create.height = __display.conn->modes[0].vdisplay;
+        __display.create.bpp = 32;
+        drmIoctl(__display.fd, DRM_IOCTL_MODE_CREATE_DUMB, &__display.create);
+        drmModeAddFB(__display.fd, __display.create.width, __display.create.height, 32, 32, __display.create.pitch, __display.create.handle, &__display.fbid);
+        drm_mode_map_dumb map;
+        map.handle = __display.create.handle;
+        drmIoctl(__display.fd, DRM_IOCTL_MODE_MAP_DUMB, &map);
+        __display.buffer = (char *)mmap(nullptr, __display.create.size, PROT_READ | PROT_WRITE, MAP_SHARED, __display.fd, map.offset);
+        memset(__display.buffer, 0, __display.create.size);
+        drmModeSetCrtc(__display.fd, crtc_id, __display.fbid, 0, 0, &conn_id, 1, &__display.conn->modes[0]);
+    }
+
+    void destoryDisplay()
+    {
+        drm_mode_destroy_dumb destroy = {};
+        drmModeRmFB(__display.fd, __display.fbid);
+        munmap(__display.buffer, __display.create.size);
+        destroy.handle = __display.create.handle;
+        drmIoctl(__display.fd, DRM_IOCTL_MODE_DESTROY_DUMB, &destroy);
+        drmModeFreeConnector(__display.conn);
+        drmModeFreeResources(__display.res);
+        close(__display.fd);
+    }
+};
--- BWLserver/display.hh
+++ BWLserver/display.hh
@@ -1,7 +1,22 @@
 #ifndef __display_hh_
 #define __display_hh_
 
+#include <iostream>
 
+namespace bwl
+{
+    /**
+     * @brief 初始化drm
+     * 
+     * @param __card_dvc 显卡设备
+     */
+    void initDisplay(std::string __card_dvc);
 
+    /**
+     * @brief 关闭drm
+     * 
+     */
+    void destoryDisplay();
+};
 
 #endif
