1f315235727994f791b79c45f734d369b7a0e8b7
pointer-to-bios <yyb1738487169@163.com>
2022-07-16 22:50:30 +0800

    add command line chioses

--- BWLserver/server.cc
+++ BWLserver/server.cc
@@ -1,14 +1,35 @@
 #include "server.hh"
 
+#define USEAGE                                                                                \
+    "usage: bwl-server [options]" << std::endl                                                \
+                                  << "The options contains:" << std::endl                     \
+                                  << "\t-h | --help\tTo print this document." << std::endl    \
+                                  << "\t-v | --version\t To print version data." << std::endl \
+                                  << "\t-c | --card\tTo choose which video card to use(card 0 as default)." << std::endl
+
 int main(int argc, char **argv)
 {
     argc--, argv++;
+    monitor_device = "0";
     for (int i = 0; i < argc; i++)
     {
-        if(std::string(argv[i]) == "-c" || std::string(argv[i]) == "--card")
+        if (std::string(argv[i]) == "-c" || std::string(argv[i]) == "--card")
         {
             i++;
-            monitor_device += argv[i];
+            monitor_device = argv[i];
+        }
+
+        if (std::string(argv[i]) == "-v" || std::string(argv[i]) == "--version")
+        {
+            std::cout << "Bad WayLand " << BWL_VERSION << std::endl
+                      << "Bad Wayland Server " << BWLS_VERSION << std::endl;
+            exit(0);
+        }
+
+        if (std::string(argv[i]) == "-h" || std::string(argv[i]) == "--help")
+        {
+            std::cout << USEAGE;
+            exit(0);
         }
     }
     switch_to_daemon();
