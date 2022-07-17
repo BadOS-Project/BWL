fe50e43d38c60b7a8dba01500b60943448b701d8
pointer-to-bios <yyb1738487169@163.com>
2022-07-17 22:57:20 +0800

    .

--- BWLserver/display.hh
+++ BWLserver/display.hh
@@ -8,35 +8,35 @@ namespace bwl
 
     /**
      * @brief 初始化drm
-     * 
+     *
      * @param __card_dvc 显卡设备
      */
     void initDisplay(std::string __card_dvc);
 
     /**
      * @brief 关闭drm
-     * 
+     *
      */
     void destoryDisplay();
 
     /**
      * @brief 获取buffer
-     * 
-     * @return void* 
+     *
+     * @return void*
      */
     void *getDrmBuffer();
 
     /**
      * @brief 获取屏幕宽度
-     * 
-     * @return uint64_t 
+     *
+     * @return uint64_t
      */
     uint64_t getDisplayWidth();
 
     /**
      * @brief 获取屏幕高度
-     * 
-     * @return uint64_t 
+     *
+     * @return uint64_t
      */
     uint64_t getDisplayHeight();
 
